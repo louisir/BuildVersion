0ee33cac468b507133a9cce0a3dc5eab3b167605
louisir <louis.androidor@gmail.com>
2023-06-08 18:44:38 +0800

    修改为文件只保存build次数，日期由命令行提供

--- bv/main.cpp
+++ bv/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[])
 
     // 获取文件路径
     std::string filePath = argv[1];
-//    std::cout << "File path: " << filePath << std::endl;
+    //    std::cout << "File path: " << filePath << std::endl;
 
     // 打开文件以读取内容
     std::ifstream file(filePath);
@@ -26,14 +26,9 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    // 读取前两行
-    std::string build_counter, build_datatime;
+    // 读build_counter
+    std::string build_counter;
     std::getline(file, build_counter);
-    std::getline(file, build_datatime);
-
-//    // 输出读取的字符串
-//    std::cout << "Line 1: " << build_counter << std::endl;
-//    std::cout << "Line 2: " << build_datatime << std::endl;
 
     // 关闭文件
     file.close();
@@ -42,23 +37,24 @@ int main(int argc, char* argv[])
     std::ofstream outputFile(filePath);
     if (!outputFile.is_open())
     {
-//        std::cout << "Failed to open file for writing." << std::endl;
+        //        std::cout << "Failed to open file for writing." << std::endl;
         return 1;
     }
 
     // 写入新的内容到前两行
     long build_counter_l = std::stol(build_counter);
     build_counter_l++;
-    outputFile << build_counter_l << std::endl;
+    outputFile << build_counter_l;
+
+    // 关闭输出文件
+    outputFile.close();
+
     std::time_t currentTime = std::time(nullptr);
     std::tm localTime;
     localtime_s(&localTime, &currentTime);
     // 格式化日期和时间
     std::stringstream ss;
     ss << std::put_time(&localTime, "%Y-%m-%d %H:%M:%S");
-    outputFile << ss.str();
-
-    // 关闭输出文件
-    outputFile.close();
+    std::cout << build_counter_l << "@" << ss.str();
     return 0;
 }
