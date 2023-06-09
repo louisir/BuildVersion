ff647009ff9cd421c5febaf3dbf02e57e85fadf0
louisir <louis.androidor@gmail.com>
2023-06-09 12:43:49 +0800

    分别加入date参数和time参数

--- bv/main.cpp
+++ bv/main.cpp
@@ -15,6 +15,9 @@ int main(int argc, char* argv[])
     }
 
     bool b_output = false;
+    bool b_version = false;
+    bool b_date = false;
+    bool b_time = false;
 
     // 遍历命令行参数
     for (int i = 1; i < argc; i++)
@@ -25,6 +28,21 @@ int main(int argc, char* argv[])
         if (arg == "--out")
         {
             b_output = true;
+        }
+
+        if (arg == "--version")
+        {
+            b_version = true;
+        }
+
+        if (arg == "--date")
+        {
+            b_date = true;
+        }
+
+        if (arg == "--time")
+        {
+            b_time = true;
         }
     }
 
@@ -73,10 +91,40 @@ int main(int argc, char* argv[])
 #ifdef __linux__
     localtime_r(&currentTime, &localTime);
 #endif
-//    // 格式化日期和时间
-//    std::stringstream ss;
+    // 格式化日期和时间
+    std::stringstream d;
 //    ss << std::put_time(&localTime, "%Y-%m-%d %H:%M:%S");
-//    std::cout << build_counter_l << " | " << ss.str() << std::endl;
-    std::cout << build_counter_l << std::endl;
+    d << std::put_time(&localTime, "%Y-%m-%d");
+
+    std::stringstream t;
+    t << std::put_time(&localTime, "%H:%M:%S");
+
+    if(b_version && b_date && b_time){
+        std::cout << build_counter_l << "|" << d.str() << "|" << t.str() << std::endl;
+        return 0;
+    }
+    if(b_version && b_date){
+        std::cout << build_counter_l << "|" << d.str() << std::endl;
+        return 0;
+    }
+    if(b_version && b_time){
+        std::cout << build_counter_l << "|" << t.str() << std::endl;
+        return 0;
+    }
+    if(b_version){
+//        std::cout << build_counter_l << std::endl;
+        std::cout << build_counter_l;
+        return 0;
+    }
+    if(b_date){
+//        std::cout << d.str() << std::endl;
+        std::cout << d.str();
+        return 0;
+    }
+    if(b_time){
+//        std::cout << t.str() << std::endl;
+        std::cout << t.str();
+        return 0;
+    }
     return 0;
 }
